c09c34ec5ae645d770d1f76c669d0b71d484f63f
Andrew <atklenner@gmail.com>
2022-04-26 19:57:44 -0700

    Start routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+
 export default function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
