89f863b9ba60374aa3b01e206d838c098175d0d6
Andrew <atklenner@gmail.com>
2022-04-28 14:53:29 -0700

    Add sign in/up routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 import Store from "./pages/Store";
 
 export default function App() {
@@ -13,6 +15,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/store" element={<Store />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,22 +4,6 @@ export default function Header() {
   return (
     <header className="container">
       <nav className="navbar" role="navigation" aria-label="main navigation">
-        {/* <div class="navbar-brand"> */}
-        {/* <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/> */}
-
-        {/* <a
-            role="button"
-            class="navbar-burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div> */}
-
         <div id="navbarBasicExample" className="navbar-menu">
           <div className="navbar-start">
             <Link to="/" className="navbar-item">
@@ -32,14 +16,15 @@ export default function Header() {
               Store
             </Link>
           </div>
-
           <div className="navbar-end">
             <div className="navbar-item">
               <div className="buttons">
-                <a className="button is-primary">
+                <Link to="/signup" className="button is-primary">
                   <strong>Sign up</strong>
-                </a>
-                <a className="button is-light">Log in</a>
+                </Link>
+                <Link to="/signin" className="button is-light">
+                  Log in
+                </Link>
               </div>
             </div>
           </div>
