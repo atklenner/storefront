0b16e2362b05de97b4150612ac8d40dde5e38512
Andrew <atklenner@gmail.com>
2022-04-28 16:05:13 -0700

    Work on about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,26 @@
-import React from "react";
-
 export default function About() {
-  return <div>About</div>;
+  return (
+    <section className="section is-large has-background-primary-light">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-half">image</div>
+          <div className="column is-half">
+            <p className="title">our story</p>
+            <p className="subtitle">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
+              accusantium sapiente tempora sed dolore esse deserunt eaque
+              excepturi, delectus error accusamus vel eligendi, omnis beatae.
+              Quisquam, dicta. Eos quod quisquam esse recusandae vitae neque
+              dolore, obcaecati incidunt sequi blanditiis est exercitationem
+              molestiae delectus saepe odio eligendi modi porro eaque in libero
+              minus unde sapiente consectetur architecto. Ullam rerum, nemo iste
+              ex, eaque perspiciatis nisi, eum totam velit saepe sed quos
+              similique amet. Ex, voluptate accusamus nesciunt totam vitae esse
+              iste.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
